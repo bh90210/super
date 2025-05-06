7102a4c7def00564c23f58e23723216ada239892
bh90210 <ktc@protonmail.ch>
2025-05-06 19:17:30 +0200

    Instant play

--- player/player.go
+++ player/player.go
@@ -86,8 +86,19 @@ func (p *Player) New(track string, volume float64) {
 		return
 	}
 
+	p.mu.Lock()
+	p.metadata.Format = filepath.Ext(track)
+	p.metadata.Path = track
+	p.metadata.streamer = &streamer{Reader: bytes.NewReader(raw)}
+	p.mu.Unlock()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
 	if errors.Is(err, fs.ErrNotExist) {
 		fmt.Println("downloading track", track)
+
+		p.metadata.streamer.download = true
 
 		raw = make([]byte, 0)
 
@@ -100,8 +111,6 @@ func (p *Player) New(track string, volume float64) {
 			return
 		}
 
-		defer conn.Close()
-
 		client := api.NewLibraryClient(conn)
 
 		response, err := client.Download(context.Background(), &api.DownloadRequest{
@@ -112,45 +121,83 @@ func (p *Player) New(track string, volume float64) {
 			return
 		}
 
-		for {
-			data, err := response.Recv()
-			if err != nil && !errors.Is(err, io.EOF) {
-				p.logger.Error("response.Recv failed", "error", err)
-				return
-			}
-
-			if data != nil {
-				fmt.Println("downloading track", track, "size", len(data.Data))
-				if len(data.Data) != 0 {
-					raw = append(raw, data.Data...)
-				}
-			}
-
-			if errors.Is(err, io.EOF) {
-				break
-			}
-		}
-
-		err = os.WriteFile(filepath.Join(p.localCache, hashedTrack), raw, 0644)
+		p.metadata.streamer.File, err = os.Create(filepath.Join(p.localCache, hashedTrack))
 		if err != nil {
-			p.logger.Error("os.WriteFile failed", "error", err)
+			p.logger.Error("os.Create failed", "error", err)
 			return
 		}
+
+		go func(conn *grpc.ClientConn, response api.Library_DownloadClient) {
+			defer conn.Close()
+
+			var mu sync.Mutex
+			var writeIndex int
+			var releaseCounter int
+			var tagRead []byte
+			for {
+				releaseCounter++
+				if releaseCounter == 3 {
+					wg.Done()
+				}
+
+				data, err := response.Recv()
+				if err != nil && !errors.Is(err, io.EOF) {
+					p.logger.Error("response.Recv failed", "error", err)
+					return
+				}
+
+				if data != nil {
+					mu.Lock()
+					n, err := p.metadata.streamer.File.WriteAt(data.Data, int64(writeIndex))
+					if err != nil {
+						p.logger.Error("file.WriteAt failed", "error", err)
+						return
+					}
+
+					err = p.metadata.streamer.File.Sync()
+					if err != nil {
+						p.logger.Error("file.Sync failed", "error", err)
+						return
+					}
+					mu.Unlock()
+
+					tagRead = append(tagRead, data.Data...)
+
+					writeIndex += n
+				}
+
+				if errors.Is(err, io.EOF) {
+					m, err := tag.ReadFrom(bytes.NewReader(tagRead))
+					if err != nil && !errors.Is(err, tag.ErrNoTagsFound) {
+						p.logger.Error("tag.ReadFrom failed", "error", err)
+						return
+					}
+
+					if !errors.Is(err, tag.ErrNoTagsFound) {
+						if m != nil {
+							pic := m.Picture()
+							if pic != nil {
+								if pic.Data != nil {
+									p.metadata.metaSize += len(pic.Data)
+								}
+							}
+						}
+					}
+					p.metadata.streamer.finished = true
+					break
+				}
+			}
+		}(conn, response)
+	} else {
+		wg.Done()
 	}
 
-	format := filepath.Ext(track)
-
-	p.mu.Lock()
-	p.metadata.Format = format
-	p.metadata.Path = track
-	p.metadata.file = bytes.NewReader(raw)
-	p.metadata.Size = len(raw)
-	p.mu.Unlock()
+	wg.Wait()
 
 	var newPlayer *oto.Player
-	switch format {
+	switch p.metadata.Format {
 	case ".mp3":
-		decodedMp3, err := mp3.NewDecoder(p.metadata.file)
+		decodedMp3, err := mp3.NewDecoder(p.metadata.streamer)
 		if err != nil {
 			p.logger.Error("mp3.NewDecoder failed", "error", err)
 		}
@@ -158,7 +205,7 @@ func (p *Player) New(track string, volume float64) {
 		newPlayer = p.otoCtx.NewPlayer(decodedMp3)
 
 	case ".wav":
-		decodedWav, err := wav.DecodeWithoutResampling(p.metadata.file)
+		decodedWav, err := wav.DecodeWithoutResampling(p.metadata.streamer)
 		if err != nil {
 			p.logger.Error("wav.DecodeWithoutResampling failed", "error", err)
 		}
@@ -166,7 +213,7 @@ func (p *Player) New(track string, volume float64) {
 		newPlayer = p.otoCtx.NewPlayer(decodedWav)
 
 	default:
-		p.logger.Error("unsupported format", "error", format)
+		p.logger.Error("unsupported format", "error", p.metadata.Format)
 		return
 	}
 
@@ -185,26 +232,28 @@ func (p *Player) New(track string, volume float64) {
 
 	// Remove the images from the size.
 	// TODO: this needs to be replaced with a proper solution.
-	f, err := os.Open(filepath.Join(p.localCache, hashedTrack))
-	if err != nil {
-		p.logger.Error("os.Open failed", "error", err)
-		return
-	}
+	if !p.metadata.streamer.download {
+		f, err := os.Open(filepath.Join(p.localCache, hashedTrack))
+		if err != nil {
+			p.logger.Error("os.Open failed", "error", err)
+			return
+		}
 
-	defer f.Close()
+		defer f.Close()
 
-	m, err := tag.ReadFrom(f)
-	if err != nil && !errors.Is(err, tag.ErrNoTagsFound) {
-		p.logger.Error("tag.ReadFrom failed", "error", err)
-		return
-	}
+		m, err := tag.ReadFrom(f)
+		if err != nil && !errors.Is(err, tag.ErrNoTagsFound) {
+			p.logger.Error("tag.ReadFrom failed", "error", err)
+			return
+		}
 
-	if !errors.Is(err, tag.ErrNoTagsFound) {
-		if m != nil {
-			pic := m.Picture()
-			if pic != nil {
-				if pic.Data != nil {
-					p.metadata.Size -= len(pic.Data)
+		if !errors.Is(err, tag.ErrNoTagsFound) {
+			if m != nil {
+				pic := m.Picture()
+				if pic != nil {
+					if pic.Data != nil {
+						p.metadata.metaSize += len(pic.Data)
+					}
 				}
 			}
 		}
@@ -217,17 +266,64 @@ type TrackMeta struct {
 	Path   string
 	Format string
 
-	file *bytes.Reader
+	metaSize int
+	streamer *streamer
 }
 
 func (p *Player) Meta() *TrackMeta {
-	// p.mu.RLock()
-	// defer p.mu.RUnlock()
+	p.mu.RLock()
+	defer p.mu.RUnlock()
 
 	return &TrackMeta{
-		Size:   p.metadata.Size,
-		Length: p.metadata.file.Len(),
+		Size:   int(p.metadata.streamer.Size()) - p.metadata.metaSize,
+		Length: p.metadata.streamer.Len(),
 		Format: p.metadata.Format,
 		Path:   p.metadata.Path,
 	}
+}
+
+type streamer struct {
+	*bytes.Reader
+	download bool
+	finished bool
+	*os.File
+	i int
+}
+
+func (s *streamer) Read(p []byte) (n int, err error) {
+	if !s.download {
+		n, err := s.Reader.Read(p)
+		return n, err
+	}
+
+	if s.finished {
+		buf := bytes.NewBuffer([]byte{})
+		// s.File.Seek(0, io.SeekStart)
+		_, err := io.Copy(buf, s.File)
+		if err != nil {
+			slog.Error("io.Copy failed", "error", err)
+			return 0, err
+		}
+
+		s.download = false
+		s.File.Close()
+
+		s.Reader = bytes.NewReader(buf.Bytes())
+		// s.Reader.Seek(int64(s.i), io.SeekStart)
+
+		n, err := s.Reader.Read(p)
+		return n, err
+	}
+
+	n, err = s.File.Read(p)
+	s.i += n
+	return n, err
+}
+
+func (s *streamer) Seek(offset int64, whence int) (int64, error) {
+	if s.download {
+		return s.File.Seek(offset, whence)
+	}
+
+	return s.Reader.Seek(offset, whence)
 }
