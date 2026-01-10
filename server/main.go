a531eb2eba75d8e379c482a2a0870d7a817a52fa
bh90210 <ktc@protonmail.ch>
2026-01-10 16:25:20 +0200

    CI/CD try 1

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 
 	"github.com/bh90210/super/api"
+	"github.com/bh90210/super/server/dupload"
 	"github.com/bh90210/super/server/library"
 	"google.golang.org/grpc"
 )
@@ -15,7 +16,7 @@ func main() {
 	path := flag.String("path", "", "Library path")
 	flag.Parse()
 
-	lis, err := net.Listen("tcp", "0.0.0.0:80")
+	lis, err := net.Listen("tcp", "0.0.0.0:8080")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -29,9 +30,15 @@ func main() {
 
 	slog.Info("Library started", "path", *path)
 
+	duploadService, err := dupload.NewService(*path)
+	if err != nil {
+		log.Fatalf("failed to create dupload service: %v", err)
+	}
+
 	// creds, err := credentials.NewClientTLSFromFile("roots.pem", "")
 	grpcServer := grpc.NewServer()
 	api.RegisterLibraryServer(grpcServer, libraryService)
+	api.RegisterDuploadServer(grpcServer, duploadService)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
